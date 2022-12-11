a6231fdb33ca6a57f00f8c696b67a02d7827a365
Noah Girdler <noahgirdler@gmail.com>
2022-12-11 16:50:19 -0500

    Adds Dialogue and Timer

--- Global.gd
+++ Global.gd
@@ -1,6 +1,21 @@
 extends Node
 
+var timer = 0
+var score = 0
 
 func _input(_event):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
+
+
+func update_score(s):
+	score += s
+	get_node("/root/UI/Score")
+	
+func update_time():
+	var t_m = floor(timer / 60.0)
+	var t_s = timer % 60
+	var t = "Time: %02d" % t_m 
+	print(t)
+	t += ":%02d" % t_s
+	get_node("/root/Game/UI/Time").text = t
---
+++ UI.gd
@@ -0,0 +1,13 @@
+extends CanvasLayer
+
+
+func _ready():
+	pass
+
+
+func _on_Timer_timeout():
+	if Global.timer > 0:
+		Global.timer -= 1
+		Global.update_time()
+
+
